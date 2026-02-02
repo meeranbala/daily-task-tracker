3383378e4eae1c35e6ad9b317b1c9990c03fb732
meeranbala <meeranbala97@gmail.com>
2026-02-02 14:00:56 +0530

    git commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import TaskTracker from './Components/taskTracker'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
